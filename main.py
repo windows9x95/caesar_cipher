592cafa37762abd248d5758617abc677576ae834
Juju <59638234+windows9x95@users.noreply.github.com>
2020-09-20 21:21:56 +0200

    I forgot to comment something....

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-SCU Systeme de Chiffement Univrsel(c) copyright 2020 
+#SCU Systeme de Chiffement Univrsel(c) copyright 2020 
 #Every tool you need in 1.5 kilooctets (ko)
 from time import *
 cst=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
